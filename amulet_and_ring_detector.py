bd739ea62d96eaf04c594aa01349bee2c09211f8
Michał Grysinski <mgrysinski@wp.pl>
2024-10-20 11:55:23 +0200

    first commit

--- amulet_and_ring_detector.py
+++ amulet_and_ring_detector.py
@@ -12,6 +12,16 @@ class AmuletAndRingDetector():
         self.pictureOfAmulet = 'C:/Users/michal/Documents/amu.PNG'
         self.pictureOfRing = 'C:/Users/michal/Documents/ring.PNG'
         self.temp_file = "C:/Users/michal/Documents/screenshot.png" 
+        #colours
+        self.life_target_color = (239, 96, 96) 
+        self.mana_target_color = (82, 79, 217)
+        #coordinates
+        self.manaY = 137
+        self.lifeY = 125
+        self.almostFullX = 1850
+        self.halfX = 1810
+        self.lessThenHalfX = 1794
+
 
     def connectToOBS(self, host, port, password):
         ws = obsws(host=host, port=port, password=password)
@@ -25,44 +35,36 @@ class AmuletAndRingDetector():
         try:
             response = connection.call(requests.GetCurrentProgramScene())
             current_scene = response.datain.get('currentProgramSceneName')
-            
-            screenshot_request = requests.SaveSourceScreenshot(
-                sourceName=current_scene,   # Nazwa źródła
-                imageFormat="png",          # Format obrazu (np. "png", "jpeg")
-                imageFilePath=self.temp_file     # Ścieżka tymczasowego pliku
-            )
+            screenshot_request = requests.SaveSourceScreenshot(sourceName=current_scene, imageFormat="png",imageFilePath=self.temp_file)
             connection.call(screenshot_request)
             return self.temp_file
-
         except Exception as e:
             print(f'Error: {e}')
 
     def find_image_on_screenshot(self, template_path, image_path, threshold=0.95):
         template = cv2.imread(template_path, cv2.IMREAD_COLOR)
         image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-
         if template is None:
             print(f'Nie można wczytać obrazu szablonu: {template_path}')
             return False, None
-    
         if image is None:
             print(f'Nie można wczytać obrazu: {image_path}')
             return False, None
         
-        # Dopasowanie szablonu do większego obrazu
+        # Match picture to screen
         result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-        # Szukanie maksymalnej wartości dopasowania
+        # search max value of match
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-        # Sprawdzenie, czy maksymalna wartość dopasowania przekracza próg
+        # check is max value of match cross a threshold
         if max_val >= threshold:
             print(f'Znaleziono obraz! Wartość dopasowania: {max_val}')
-            return True, max_loc  # Zwracamy informację o sukcesie i pozycję (x, y)
+            return True, max_loc  # Return True + item position (x, y)
         else:
             print(f'Nie znaleziono obrazu. Wartość dopasowania: {max_val}')
             return False, None
 
-    def compare(self, stop_event):
+    def compare(self, stop_event, ss, might):
         conn = self.connectToOBS(self.host, self.port, self.password)
 
         while not stop_event.is_set():
@@ -70,20 +72,20 @@ class AmuletAndRingDetector():
 
             if screenShotFromObs is not None:
                 ring = self.find_image_on_screenshot(screenShotFromObs, self.pictureOfRing)
-                if ring[0] == True:
+                if ring[0] == True and might == 1:
                     print(f'znaleziono ring: {ring}')
                     pyautogui.press('2')
                 amulet = self.find_image_on_screenshot(screenShotFromObs, self.pictureOfAmulet)
-                if amulet[0] == True:
+                if amulet[0] == True and ss == 1:
                     print(f'znaleziono amulet: {amulet}')
                     pyautogui.press('1')
             time.sleep(1)
         
         conn.disconnect()
 
-    def startAmuAndRingEvent(self, stop_event):
+    def startAmuAndRingEvent(self, stop_event, ss, might):
         stop_event = threading.Event()
-        thread  = threading.Thread(target=self.compare, args=(stop_event,))
+        thread  = threading.Thread(target=self.compare, args=(stop_event, ss, might))
         thread .start()
         return thread , stop_event
     
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import customtkinter as ck
 from datetime import datetime
 from amulet_and_ring_detector import AmuletAndRingDetector
-import time
 
 ck.set_appearance_mode('Dark')
 ck.set_default_color_theme('dark-blue')
@@ -13,36 +12,65 @@ class App(ck.CTk):
         #Configure window
         self.geometry('400x450')
         self.resizable(False, False)
-        self.title('Gryzak bot 1.0')
+        self.title('GryzakBot 1.0')
 
         #STATIC variable
-        self.OBSCONNECTED = False
-        self.host = "localhost"
         self.amuRingThread = None
         self.AmuRingStop_event = None
 
         #Tab configuration
         self.tab = ck.CTkTabview(self, width=400, height=300)
         self.tab.grid(row=0, column=0)
-        self.tab.add('AutoHealing')
-        self.tab.add('Ring&Amu')
+        self.tab.add('Config')
+        self.tab.add('BOT')
         self.logBox = ck.CTkTextbox(self, state='disabled', width=400, height=150)
         self.logBox.grid(row=1, column=0)
 
-        #AutoHealing configuration
+        #Configuration 
+        self.ssLebale = self.create_label(self.tab.tab('Config'), text='Stone Skin')
+        self.ssLebale.grid(row=0, column=0)
+        self.ssButton = self.create_button(self.tab.tab('Config'), text='Assigne', command=lambda: self.start_key_assignment(self.ssButton))
+        self.ssButton.grid(row=0, column=1)
+        self.mightLebale = self.create_label(self.tab.tab('Config'), text='Might Ring')
+        self.mightLebale.grid(row=0, column=2)
+        self.mightButton = self.create_button(self.tab.tab('Config'), text='Assigne', command=lambda: self.start_key_assignment(self.mightButton))
+        self.mightButton.grid(row=0, column=3)
+        self.sioLebale = self.create_label(self.tab.tab('Config'), text='Sio')
+        self.sioLebale.grid(row=1, column=0)
+        self.sioButton = self.create_button(self.tab.tab('Config'), text='Assigne', command=lambda: self.start_key_assignment(self.sioButton))
+        self.sioButton.grid(row=1, column=1)
+        self.uhLebale = self.create_label(self.tab.tab('Config'), text='Uh')
+        self.uhLebale.grid(row=1, column=2)
+        self.uhButton = self.create_button(self.tab.tab('Config'), text='Assigne', command=lambda: self.start_key_assignment(self.uhButton))
+        self.uhButton.grid(row=1, column=3)
+        self.exuraVitaLebale = self.create_label(self.tab.tab('Config'), text='Exura Vita')
+        self.exuraVitaLebale.grid(row=2, column=0)
+        self.exuraVitaButton = self.create_button(self.tab.tab('Config'), text='Assigne', command=lambda: self.start_key_assignment(self.exuraVitaButton))
+        self.exuraVitaButton.grid(row=2, column=1)
+        self.exuraMaxVitaLebale = self.create_label(self.tab.tab('Config'), text='Exura Max Vita')
+        self.exuraMaxVitaLebale.grid(row=2, column=2)
+        self.exuraMaxVitaButton = self.create_button(self.tab.tab('Config'), text='Assigne', command=lambda: self.start_key_assignment(self.exuraMaxVitaButton))
+        self.exuraMaxVitaButton.grid(row=2, column=3)
 
-
-        #Ring&Amu configuration
-        self.stoneskincheckbox = self.create_checkbox(self.tab.tab('Ring&Amu'), text='Stone Skin')
-        self.stoneskincheckbox.grid(row=0, column=0, pady=5)
-        self.mightringcheckbox = self.create_checkbox(self.tab.tab('Ring&Amu'), text='Might Ring')
-        self.mightringcheckbox.grid(row=1, column=0, pady=5)
-        self.amuAndRingEnableButton = self.create_button(self.tab.tab('Ring&Amu'), text='Start', command=self.amuAndRingEnableButtonFunc)
-        self.amuAndRingEnableButton.grid(row=2, column=0, pady=5, padx=5)
-        self.amuAndRingDisableButton = self.create_button(self.tab.tab('Ring&Amu'), text='Stop', state='disabled', command=self.amuAndRingDisableButtonFunc)
-        self.amuAndRingDisableButton.grid(row=2, column=1, pady=5, padx=5 )
+        #BOTconfiguration
+        self.amuAndRingLabel = self.create_label(self.tab.tab('BOT'), text="Auto Ring and Amulet:")
+        self.amuAndRingLabel.grid(row=0, column=0, sticky="w")
+        self.stoneskincheckbox = self.create_checkbox(self.tab.tab('BOT'), text='Stone Skin')
+        self.stoneskincheckbox.grid(row=1, column=0, sticky="w", pady=2)
+        self.mightringcheckbox = self.create_checkbox(self.tab.tab('BOT'), text='Might Ring')
+        self.mightringcheckbox.grid(row=2, column=0, pady=2, sticky="w")
+        self.healingLabel = self.create_label(self.tab.tab('BOT'), text="Auto Healing and Mana:")
+        self.healingLabel.grid(row=3, column=0, sticky="w") 
+        self.autoHealing = self.create_checkbox(self.tab.tab('BOT'), text='Healing')
+        self.autoHealing.grid(row=4, column=0, pady=2, sticky="w")
+        self.autoMana = self.create_checkbox(self.tab.tab('BOT'), text='Mana')
+        self.autoMana.grid(row=5, column=0, pady=2, sticky="w")
+        self.startButton = self.create_button(self.tab.tab('BOT'), text='Start', command=self.startButton)
+        self.startButton.grid(row=6, column=0, pady=2, padx=5, sticky="e")
+        self.stopButton = self.create_button(self.tab.tab('BOT'), text='Stop', state='disabled', command=self.stopButton)
+        self.stopButton.grid(row=6, column=1, pady=2, padx=5 )
     
-    def create_button(self, parent, text, command=None, state='normal', width=100, height=40):
+    def create_button(self, parent, text, command=None, state='normal', width=70, height=30):
         return ck.CTkButton(parent, text=text, command=command, state=state, width=width, height=height)
     
     def create_label(self, parent, text, width=100, height=40):
@@ -53,6 +81,19 @@ class App(ck.CTk):
     
     def create_checkbox(self, parent, text):
         return ck.CTkCheckBox(parent, text=text)
+
+    # Assigne button to key
+    def on_assign_key_press(self, event, button):
+        print(button.winfo_name())
+        assigned_key = event.keysym  # Pobranie nazwy naciśniętego klawisza
+        button.configure(text=assigned_key)  # Zmiana tekstu klikniętego przycisku
+        app.unbind("<KeyPress>")  # Odłączenie nasłuchiwania klawiatury
+        self.logging(f"Key assigned: {assigned_key}")
+
+    # start listening for keyboard
+    def start_key_assignment(self, button):
+        self.logging("Press key...")
+        app.bind("<KeyPress>", lambda event: self.on_assign_key_press(event, button)) 
     
     def logging(self, message):
         self.now = datetime.now()
@@ -61,23 +102,21 @@ class App(ck.CTk):
         self.logBox.insert('0.0', f"{self.dt_string} - {message}\n")
         self.logBox.configure(state='disabled')
 
-    def amuAndRingEnableButtonFunc(self):
-        self.amuAndRingEnableButton.configure(state='disabled')
-        self.amuAndRingDisableButton.configure(state='normal')
+    def startButton(self):
+        self.startButton.configure(state='disabled')
+        self.stopButton.configure(state='normal')
         stoneSkinStatus = self.stoneskincheckbox.get()
         mightRingStatus = self.mightringcheckbox.get()
-
         obs_instance = AmuletAndRingDetector()
-        self.amuRingThread, self.AmuRingStop_event = obs_instance.startAmuAndRingEvent(self)
-        self.logging(f"Started auto amulet and ring")
+        self.amuRingThread, self.AmuRingStop_event = obs_instance.startAmuAndRingEvent(self, stoneSkinStatus, mightRingStatus)
+        self.logging(f"Program started you enabled:")
 
-    def amuAndRingDisableButtonFunc(self):
-        self.amuAndRingEnableButton.configure(state='normal')
-        self.amuAndRingDisableButton.configure(state='disabled')
+    def stopButton(self):
+        self.startButton.configure(state='normal')
+        self.stopButton.configure(state='disabled')
         obs_instance = AmuletAndRingDetector()
         obs_instance.stopAmuAndRingEvent(self.AmuRingStop_event, self.amuRingThread)
         self.logging(f"Stopped auto amulet and ring")
-
 
 if __name__ == "__main__":
     app = App()
